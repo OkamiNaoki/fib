0b5d0db312bb1b6c5b449d853587920e6239ad88
okami naoki <72971126+OkamiNaoki@users.noreply.github.com>
2023-08-15 14:02:24 +0900

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views import View
 
-def fibonacci(request):
-    n = 10  # Number of Fibonacci numbers to generate
-    fibonacci_sequence = calculate_fibonacci(n)
-    return render(request, 'fibonacci/fibonacci.html', {'fibonacci_sequence': fibonacci_sequence})
-
-def calculate_fibonacci(n):
-    sequence = [0, 1]
-    for i in range(2, n):
-        next_number = sequence[i - 1] + sequence[i - 2]
-        sequence.append(next_number)
-    return sequence
+class FibonacciView(View):
+    def get(self, request, *args, **kwargs):
+        try:
+            n = int(request.GET.get('n', 0))
+            if n < 0:
+                return HttpResponseBadRequest("Invalid value for n")
+            
+            result = self.calculate_fibonacci(n)
+            return JsonResponse({'result': result})
+        except ValueError:
+            return self.error_response(status=400, message="Bad request.")
+    
+    def calculate_fibonacci(self, n):
+        fib = [0, 1]
+        for i in range(2, n + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
+        return fib[n]
+    
+    def error_response(self, status, message):
+        return JsonResponse({'status': status, 'message': message}, status=status)
